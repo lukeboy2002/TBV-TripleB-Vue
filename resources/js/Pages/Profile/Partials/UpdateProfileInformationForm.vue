e22d08c04e86f93ccedc4a868fabacce8ad95909
Lukeboy_2002 <35039280+lukeboy2002@users.noreply.github.com>
2024-05-18 22:52:21 +0200

    Update UpdateProfileInformationForm.vue

--- resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
+++ resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
@@ -130,7 +130,7 @@ const clearPhotoFileInput = () => {
 
             <!-- Name -->
             <div class="col-span-6 sm:col-span-4">
-                <InputLabel for="name" value="Name" />
+                <InputLabel for="name" value="Username" />
                 <TextInput
                     id="username"
                     v-model="form.username"
